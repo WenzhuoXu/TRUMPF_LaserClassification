3504113f5303808729f257a5ac3f27ce4a7e3072
WenzhuoXu <xuwenzhuo46@126.com>
2022-02-18 10:36:28 +0800

    数据集处理

--- testCodes/datasetGen.py
+++ testCodes/datasetGen.py
@@ -3,7 +3,8 @@ import os
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
-from PIL import Image
+
+# from PIL import Image
 
 data_dir = 'F:/Work/Bachelor-thesis/Data/data2021_ori/90_ori'
 
@@ -12,20 +13,29 @@ if __name__ == "__main__":
         for i in range(1, 23):
             for filename in os.listdir(data_dir + '/' + str(i)):
                 img = cv2.imread(data_dir + '/' + str(i) + '/' + filename)
+                plt.figure("originalImage")
+                plt.imshow(img)
+
                 f = np.fft.fft2(img)
                 fshift = np.fft.fftshift(f)
                 magnitude_spectrum = 20 * np.log(np.abs(fshift))
 
                 rows, cols = img.shape[:2]
                 crow, ccol = int(rows / 2), int(cols / 2)
-                fshift[(crow - 500):(crow + 500), (ccol - 500):(ccol + 500)] = 0
+                # plt.figure("prefourier")
+                # plt.imshow(fshift)
+                fshift[(crow - 30):(crow + 30), (ccol - 30):(ccol + 30)] = 0
+                # plt.figure("aftfourier")
+                # plt.imshow(fshift)
                 f_ishift = np.fft.ifftshift(fshift)
                 img_back = np.fft.ifft2(f_ishift)
                 img_back = np.abs(img_back)
 
-                plt.figure()
+                plt.figure("img_back")
                 plt.imshow(img_back)
-                imsave = Image.fromarray(np.uint8(img_back))
-                imsave.save('F:/Work/Bachelor-thesis/Data/data_highfreq/' + filename)
+                plt.axis('off')
+                plt.savefig('F:/Work/Bachelor-thesis/Data/data_highfreq/' + filename, transparent=True)
+                # imsave = Image.fromarray(np.uint8(img_back))
+                # imsave.save('F:/Work/Bachelor-thesis/Data/data_highfreq/' + filename)
                 index.write(filename + '\n')
                 print(filename)
