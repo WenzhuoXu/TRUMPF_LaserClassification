fdba585c0443bd722fcefc1af2e8483df77203d9
WenzhuoXu <xuwenzhuo46@126.com>
2022-02-22 13:16:18 +0800

    数据集处理

--- testCodes/datasetGen.py
+++ testCodes/datasetGen.py
@@ -3,18 +3,20 @@ import os
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 # from PIL import Image
 
 data_dir = 'F:/Work/Bachelor-thesis/Data/data2021_ori/90_ori'
 
 if __name__ == "__main__":
+    datasheet = pd.read_excel('F:/Work/Bachelor-thesis/Data/datasheet.xlsx')
     with open(data_dir + "index.txt", "w") as index:
         for i in range(1, 23):
             for filename in os.listdir(data_dir + '/' + str(i)):
                 img = cv2.imread(data_dir + '/' + str(i) + '/' + filename)
-                plt.figure("originalImage")
-                plt.imshow(img)
+                # plt.figure("originalImage")
+                # plt.imshow(img)
 
                 f = np.fft.fft2(img)
                 fshift = np.fft.fftshift(f)
@@ -31,11 +33,18 @@ if __name__ == "__main__":
                 img_back = np.fft.ifft2(f_ishift)
                 img_back = np.abs(img_back)
 
+                img_back[0:30, ] = 255
+                img_back[(rows - 30):rows, ] = 255
+
                 plt.figure("img_back")
                 plt.imshow(img_back)
                 plt.axis('off')
                 plt.savefig('F:/Work/Bachelor-thesis/Data/data_highfreq/' + filename, transparent=True)
+
                 # imsave = Image.fromarray(np.uint8(img_back))
                 # imsave.save('F:/Work/Bachelor-thesis/Data/data_highfreq/' + filename)
-                index.write(filename + '\n')
+                index.write(filename + '\t' + str(datasheet.loc[i - 1, 'focus']) + '\t' + str(
+                    datasheet.loc[i - 1, 'pressure']) + '\n')
+
                 print(filename)
+    datasheet.close()
