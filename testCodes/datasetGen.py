c69b693e396a89afa20d6fb4d5bb3d36bbff5cce
WenzhuoXu <xuwenzhuo46@126.com>
2022-02-22 20:49:35 +0800

    数据集处理

--- testCodes/datasetGen.py
+++ testCodes/datasetGen.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 import cv2
 import matplotlib.pyplot as plt
@@ -11,40 +12,45 @@ data_dir = 'F:/Work/Bachelor-thesis/Data/data2021_ori/90_ori'
 
 if __name__ == "__main__":
     datasheet = pd.read_excel('F:/Work/Bachelor-thesis/Data/datasheet.xlsx')
-    with open(data_dir + "index.txt", "w") as index:
-        for i in range(1, 23):
-            for filename in os.listdir(data_dir + '/' + str(i)):
-                img = cv2.imread(data_dir + '/' + str(i) + '/' + filename)
-                # plt.figure("originalImage")
-                # plt.imshow(img)
+    testing_index = open(data_dir + "_testing_index.txt", "w")
+    training_index = open(data_dir + "_training_index.txt", "w")
+    for i in range(1, 23):
+        for filename in os.listdir(data_dir + '/' + str(i)):
+            img = cv2.imread(data_dir + '/' + str(i) + '/' + filename)
+            # plt.figure("originalImage")
+            # plt.imshow(img)
 
-                f = np.fft.fft2(img)
-                fshift = np.fft.fftshift(f)
-                magnitude_spectrum = 20 * np.log(np.abs(fshift))
+            f = np.fft.fft2(img)
+            fshift = np.fft.fftshift(f)
+            magnitude_spectrum = 20 * np.log(np.abs(fshift))
 
-                rows, cols = img.shape[:2]
-                crow, ccol = int(rows / 2), int(cols / 2)
-                # plt.figure("prefourier")
-                # plt.imshow(fshift)
-                fshift[(crow - 30):(crow + 30), (ccol - 30):(ccol + 30)] = 0
-                # plt.figure("aftfourier")
-                # plt.imshow(fshift)
-                f_ishift = np.fft.ifftshift(fshift)
-                img_back = np.fft.ifft2(f_ishift)
-                img_back = np.abs(img_back)
+            rows, cols = img.shape[:2]
+            crow, ccol = int(rows / 2), int(cols / 2)
+            # plt.figure("prefourier")
+            # plt.imshow(fshift)
+            fshift[(crow - 30):(crow + 30), (ccol - 30):(ccol + 30)] = 0
+            # plt.figure("aftfourier")
+            # plt.imshow(fshift)
+            f_ishift = np.fft.ifftshift(fshift)
+            img_back = np.fft.ifft2(f_ishift)
+            img_back = np.abs(img_back)
 
-                img_back[0:30, ] = 255
-                img_back[(rows - 30):rows, ] = 255
+            img_back[0:30, ] = 255
+            img_back[(rows - 30):rows, ] = 255
 
-                # plt.figure("img_back")
-                # plt.imshow(img_back)
-                plt.axis('off')
-                plt.savefig('F:/Work/Bachelor-thesis/Data/data_highfreq/' + filename, transparent=True)
+            plt.figure("img_back")
+            plt.imshow(img_back)
+            plt.axis('off')
+            plt.savefig('F:/Work/Bachelor-thesis/Data/data_highfreq/' + filename, transparent=True)
 
-                # imsave = Image.fromarray(np.uint8(img_back))
-                # imsave.save('F:/Work/Bachelor-thesis/Data/data_highfreq/' + filename)
-                index.write(filename + '\t' + str(datasheet.loc[i - 1, 'focus']) + '\t' + str(
+            # imsave = Image.fromarray(np.uint8(img_back))
+            # imsave.save('F:/Work/Bachelor-thesis/Data/data_highfreq/' + filename)
+
+            if random.random() < 0.8:
+                training_index.write(filename + '\t' + str(datasheet.loc[i - 1, 'focus']) + '\t' + str(
+                    datasheet.loc[i - 1, 'pressure']) + '\n')
+            else:
+                testing_index.write(filename + '\t' + str(datasheet.loc[i - 1, 'focus']) + '\t' + str(
                     datasheet.loc[i - 1, 'pressure']) + '\n')
 
-                print(filename)
-    datasheet.close()
+            print(filename)
--- testCodes/qualityTrain.py
+++ testCodes/qualityTrain.py
@@ -1,2 +1,45 @@
-import numpy as np
-import torch
+import os
+
+import pandas as pd
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from torchvision.io import read_image
+
+training_index = 'F:/Work/Bachelor-thesis/Data/data2021_ori/90_ori_training_index.txt'
+testing_index = 'F:/Work/Bachelor-thesis/Data/data2021_ori/90_ori_testing_index.txt'
+img_dir = 'F:/Work/Bachelor-thesis/Data/data_highfreq'
+
+
+class LaserCutEvalDataset(Dataset):
+    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
+        self.img_labels = pd.read_table(annotations_file, sep='\t')
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.img_labels)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        image = read_image(img_path)
+        label = self.img_labels.iloc[idx, 1:4]
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            image = self.target_transform(image)
+        return image, label
+
+
+train_transforms = transforms.Compose([
+    transforms.ToTensor(),
+])
+test_transforms = transforms.Compose([
+    transforms.ToTensor(),
+])
+
+training_data = LaserCutEvalDataset(training_index, img_dir, train_transforms)
+testing_data = LaserCutEvalDataset(testing_index, img_dir, test_transforms)
+
+training_dataloader = DataLoader(training_data, batch_size=16, shuffle=True)
+testing_dataloader = DataLoader(testing_data, batch_size=16, shuffle=True)
