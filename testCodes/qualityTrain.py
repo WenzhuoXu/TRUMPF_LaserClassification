1c9ac57ed569b2df6af282befdc006a3b9bbe4ba
WenzhuoXu <xuwenzhuo46@126.com>
2022-02-24 18:41:21 +0800

    训练集准备

--- testCodes/qualityTrain.py
+++ testCodes/qualityTrain.py
@@ -159,6 +159,7 @@ train_transforms = transforms.Compose([
 ])
 
 test_transforms = transforms.Compose([
+    transforms.ToPILImage(),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
